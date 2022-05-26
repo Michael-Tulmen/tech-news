864aaddcc356f49981f80353cff435b75ec37f7e
Michael Tulmen <michaeltulmen@gmail.com>
2022-05-25 21:25:22 -0500

    13.2 end

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 //create user model
 class User extends Model {}
@@ -39,22 +40,28 @@ User.init(
                 //password must be at least four characters long
                 len: [4]
             }
-        }
+        },
     },
     {
-        // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
-        //Pass in our imported sequelize connection (the direct connection to our database)
+        hooks: {
+          // set up beforeCreate lifecycle "hook" functionality
+          async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+          },
+    
+          async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+          }
+        },
         sequelize,
-        //don't automatically create createdAt/updatedAt timestamp fields
         timestamps: false,
-        // don't pluralize name of database table
         freezeTableName: true,
-        // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
-        underscored:true,
-        // make it so our model name stays lowercase in the database
-        modelName:'user'
-    }
-);
+        underscored: true,
+        modelName: 'user'
+      }
+    );
+    
 
 module.exports = User;
