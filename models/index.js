26ae58a75a6e9b642ac38376a8d5def8448df327
Michael Tulmen <michaeltulmen@gmail.com>
2022-05-26 22:51:54 -0500

    vote model created

--- models/index.js
+++ models/index.js
@@ -1,4 +1,19 @@
-const User = require("./User");
+// import all models
 const Post = require("./Post");
+const User = require("./User");
+const Vote = require("./Vote");
+
+// create associations
+User.hasMany(Post, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "user_id",
+});
+
+Post.belongsTo(User, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "user_id",
+});
 
 module.exports = { User, Post };
